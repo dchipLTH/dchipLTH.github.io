6099b034d3882927f36ef07474af1f315f7b43fc
Josefin W <jo2466we-s@student.lu.se>
2024-11-25 12:49:32 +0100

    Added website

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
+    site: 'https://dchiplth.github.io',
+    base: 'hemsida',
     integrations: [mdx(), sitemap(), tailwind()],
 });
