7199837c33c1e54a45d544096848eb2a427245bf
hannaarendt <hannaarendt02@hotmail.com>
2024-12-11 22:41:42 +0100

    fix some small grammar mistakes

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'D-chip';
+export const SITE_TITLE = 'D-Chip';
 export const SITE_DESCRIPTION = 'Välkommen till vår hemsida!';
 export const MAIL = 'd-chipstyrelse@dsek.se';
