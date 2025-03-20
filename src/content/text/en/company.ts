7aa35311260d9d7f027feae078b694c4787e76fb
hannaarendt <hannaarendt02@hotmail.com>
2025-03-20 12:13:38 +0100

    refactor company page

---
+++ src/content/text/en/company.ts
@@ -0,0 +1,47 @@
+export const text = {
+    title: "Do you want to collaborate with us?",
+    info: `D-Chip is a network for women and non-binary students and alumni who are studying or have studied 
+                for a Master of Science in Engineering in Computer Science or Information and Communication Technology 
+                at Lund University’s Faculty of Engineering. Through a collaboration with D-Chip, your company can easily 
+                reach this group and build a relationship with them before they enter the job market.`,
+    title2: "How can we meet?",
+    indchip: "At D-Chip, we organize a variety of exciting events to connect our members with the business world and create meaningful networks. Here are some examples of what we can offer:",
+    together: "Together, we can find the perfect activities to create memorable experiences and build strong relationships with the engineers of the future.",
+    contactus: "Contact us",
+    collaboration: "Are you interested in collaborating, or do you want to learn more? Don't hesitate to contact us. We look forward to hearing from you and creating a valuable partnership!",
+    contactperson: "Head of Corporate Relations"
+};
+
+export const cards = [
+    {
+        card: "Lunch lecture"
+    },
+    {
+        card: "Chocolate tasting"
+    },
+    {
+        card: "Sushi night"
+    },
+    {
+        card: "Company visit"
+    },
+    {
+        card: "Pub"
+    },
+    {
+        card: "Coffee break"
+    },
+    {
+        card: "Dinner at at restaurant"
+    },
+    {
+        card: "CV writing"
+    },
+    {
+        card: "Wine tasting"
+    },
+    {
+        card: "Case night"
+    }
+];
+
---
+++ src/content/text/sv/company.ts
@@ -0,0 +1,48 @@
+export const text = {
+    title: "Vill ni samarbeta med oss?",
+    info: `D-Chip är ett nätverk för kvinnor och icke-binära studenter och alumner som läser eller har läst 
+    till civilingenjör i datateknik eller information- och kommunikationsteknik vid Lunds Tekniska Högskola. 
+    Genom ett samarbete med D-Chip kan ert företag enkelt nå ut  till denna grupp och bygga upp en relation 
+    med dem redan innan de når arbetsmarknaden.`,
+    published: "Publicerat",
+    title2: "Hur kan vi mötas?",
+    indchip: "Vi i D-Chip anordnar en rad spännande evenemang för att koppla samman våra medlemmar med näringslivet och skapa meningsfulla nätverk. Här är några exempel på vad vi kan erbjuda:",
+    together: "Tillsammans kan vi hitta de perfekta aktiviteterna för att skapa minnesvärda upplevelser och bygga starka relationer med framtidens ingenjörer.",
+    contactus: "Kontakta oss",
+    collaboration: "Är ni intresserade av att samarbeta eller vill ni veta mer? Tveka inte att kontakta oss. Vi ser fram emot att höra från er och skapa ett givande samarbete!",
+    contactperson: "Näringslivsansvarig"
+};
+
+export const cards = [
+    {
+        card: "Lunchföreläsning"
+    },
+    {
+        card: "Chokladprovning"
+    },
+    {
+        card: "Sushikväll"
+    },
+    {
+        card: "Studiebesök"
+    },
+    {
+        card: "Pub"
+    },
+    {
+        card: "Fika"
+    },
+    {
+        card: "Middag på restaurang"
+    },
+    {
+        card: "CV-skrivning"
+    },
+    {
+        card: "Vinprovning"
+    },
+    {
+        card: "Casekväll"
+    }
+];
+
