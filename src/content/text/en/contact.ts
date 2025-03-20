2978db76feb9bc97892489b9a7c832b830f743ec
hannaarendt <hannaarendt02@hotmail.com>
2025-03-20 12:14:00 +0100

    refactor contact page

---
+++ src/content/text/en/contact.ts
@@ -0,0 +1,7 @@
+export const text = {
+    title: "Contact us",
+    question: "Do you have a question?",
+    mailUs: "Don't hestitate to reach out! Send us an e-mail at",
+    mailUsCont: "or contact someone in the board directly.",
+    sendMail: "Send mail"
+};
