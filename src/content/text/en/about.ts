6c92da99de7f989d902a00fc07ef7e10422ec88e
hannaarendt <hannaarendt02@hotmail.com>
2025-03-20 12:12:45 +0100

    refactor about page

---
+++ src/content/text/en/about.ts
@@ -0,0 +1,30 @@
+export const text = {
+    title: "What is D-Chip?",
+    description: `D-Chip is an independent association that was founded in the fall of 1995. 
+            Our purpose is to promote the well-being and safety of women and non-binary individuals 
+            in the D-guild of TLTH at Lund University's Faculty of Engineering (LTH), 
+            as well as to strengthen their engagement.
+
+            In the fall we organize a welcoming lunch the day before the semester starts and a mentorship program for all new students,
+             as well as a variety of other activities and events throughout the remainder of the year.
+
+            As a "nolla", you are called a microchip. Once you are elevated to a first-year student, you can call yourself D-Chip.  
+            Alumni are also referred to as Super Hacker Babes, a tradition that has been in place since the association
+             was founded 30 years ago. A D-Chip, in addition to having a pink (Computer Engineering) or 
+             purple (Information and Communication Technology) tassel, also has a pink ribbon.`,
+    
+    board: "The Board",
+    volunteers: "Volunteers",
+    auditors: "Auditors",
+    valberedning: "The Nomination Comittee",
+    jubilee: "Jubilee Group 2025",
+
+    auditorsDescription: "The association shall have at least one, and no more than two, auditors. The auditors must be legal adults, Swedish citizens, members of the D-section at Lund University of Technology, and possess the financial knowledge required for the task.",
+    valberedningDescription: "The Nomination Committee consists of at least two representatives from the Computer Sciene and Information and Communication Technology programmes.",
+    jubileeDescription: "The Jubilee Group is responsible for planning the 30th anniversary of D-Chip 2025.",
+
+    engage: "Would you like to join the association?",
+    engageDescription: "Please contact us through our social medias or mail:",
+    button: "Contact us"
+}
+  
---
+++ src/content/text/sv/board.ts
@@ -0,0 +1,134 @@
+const president = [ 
+    {
+        name: "Hilma Larsson",
+        position: "Ordförande",
+        class: "C23",
+        description: "Ordföranden har i uppgift att i samråd med styrelsen utöva ledningen av föreningen, samt att i officiella sammanhang representera föreningen.",
+        contact: "d-chipordforande@dsek.se",
+        image: "/images/board/NoPicture.jpg" // Add the actual image path
+      }
+    ]
+    const teamMembers = [
+      {
+        name: "Hanna Arendt",
+        position: "Vice Ordförande",
+        class: "C23",
+        description: "Vice ordförande har i uppgift att representera föreningen i ordförandes frånvaro samt utföra de uppgifter, som ligger i föreningens ändamål, vilka delegeras av ordförande.",
+        contact: "d-chipvordforande@dsek.se",
+        image: "/images/board/hanna.jpg" // image path
+      },
+      {
+        name: "Ylva Lengborg",
+        position: "Eventansvarig",
+        class: "C23",
+        description: "Eventansvarig har huvudansvaret för de event som anordnas av föreningen och att handha föreningens säljbara artiklar.",
+        contact: "d-chipevent@dsek.se",
+        image: "/images/board/ylva.jpg" // image path
+      },
+      {
+        name: "Lisa Clevestam",
+        position: "Informationsansvarig",
+        class: "C23",
+        description: "Informationsansvarig har i uppgift att vara ansvarig för föreningens hemsida och sociala medier. Hen ska även ansvara för distributionen av intern information rörande D-Chip.",
+        contact: "d-chipinfo@dsek.se",
+        image: "/images/board/NoPicture.jpg" // image path
+      },
+      {
+        name: "Elly Hedin Oleniusson",
+        position: "Näringslivsansvarig",
+        class: "D23",
+        description: "Näringslivsansvarig har i uppgift att förvalta och initiera föreningens företagskontakter.",
+        contact: "d-chipnaringsliv@dsek.se",
+        image: "/images/board/elly.jpg" // image path
+      }, 
+      {
+        name: "Vivea Upadhyaya",
+        position: "Ekonomiansvarig",
+        class: "D24",
+        description: "Ekonomiansvarig har i uppgift att i samråd med styrelsen ha kontroll över föreningens pengar.",
+        contact: "d-chipekonomi@dsek.se",
+        image: "/images/board/NoPicture.jpg" // image path
+      },
+      {
+      name: "Maya Frank",
+        position: "Sekreterare",
+        class: "C24",
+        description: "Sekreteraren har i uppgift att protokollföra alla föreningsmöten och styrelsemöten samt se till att dessa protokoll justeras och offentliggörs på lämpligt sätt.",
+        contact: "d-chipsekreterare@dsek.se",
+        image: "/images/board/NoPicture.jpg" // image path
+      },
+    
+    ];
+    const revisorer = [
+    {
+        name: "Kristina Sedelius",
+        position: "Revisor",
+        class: "D21",
+        description: "A passionate learner and aspiring developer.",
+        contact: "d-chiprevisor@dsek.se",
+        image: "/images/board/hanna.jpg" // Add the actual image path
+      },
+      {
+        name: "Ester Nielsen Gulis",
+        position: "Revisor",
+        class: "C22",
+        description: "A passionate learner and aspiring developer.",
+        contact: "d-chiprevisor@dsek.se",
+        image: "/images/board/hanna.jpg" // Add the actual image path
+      },
+    ];
+    const valberedning = [
+    {
+        name: "Clara Schagerström",
+        position: "Valberedning",
+        class: "D23",
+        description: "A passionate learner and aspiring developer.",
+        contact: "d-chiprevisor@dsek.se",
+        image: "/images/board/hanna.jpg" // Add the actual image path
+      },
+      {
+        name: "Elsa Ryrlén",
+        position: "Valberedning",
+        class: "C21",
+        description: "Föreningens valberedning utgörs av en representant från var och en av de fyra senaste årskurserna på Datateknik och Infocom. Valberedningen utgörs av minst två representanter.",
+        contact: "d-chiprevisor@dsek.se",
+        image: "/images/board/hanna.jpg" // Add the actual image path
+      },
+      {
+        name: "Julia Blomgren",
+        position: "Valberedning",
+        class: "D21",
+        description: "A passionate learner and aspiring developer.",
+        contact: "d-chiprevisor@dsek.se",
+        image: "/images/board/hanna.jpg" // Add the actual image path
+      }
+    ];
+    
+    const jubileegroup = [
+    {
+        name: "Anna Hallin",
+        class: "C23",
+      },
+      {
+        name: "Alice Ward",
+        class: "D24",
+      },
+      {
+        name: "Alma Karlsson",
+        class: "C23",
+      },
+      {
+        name: "Astrid Mildner",
+        class: "C23",
+      },
+      {
+        name: "Ellen Persson",
+        class: "C23",
+      },
+      {
+      name: "Linn Eripe",
+        class: "C23",
+      }
+    ];
+
+export const board = { president, teamMembers, revisorer, valberedning, jubileegroup };
