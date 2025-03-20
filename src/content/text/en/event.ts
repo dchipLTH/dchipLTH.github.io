4301e99dae831aff1ee9e5c31c46e1ca347ecdc9
hannaarendt <hannaarendt02@hotmail.com>
2025-03-20 12:14:15 +0100

    refactor event page

---
+++ src/content/text/en/event.ts
@@ -0,0 +1,4 @@
+export const text = {
+    title: "Events",
+    info: "Here you can see upcoming events. You can find registration and more information in the event descriptions."
+};
---
+++ src/content/text/sv/event.ts
@@ -0,0 +1,4 @@
+export const text = {
+    title: "Evenemang",
+    info: "Här kan du se våra kommande event. Anmälan och mer information hittar du i eventbeskrivningarna."
+};
