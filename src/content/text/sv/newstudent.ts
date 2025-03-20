a5526cc05392692782629b188c20602ecb17433c
hannaarendt <hannaarendt02@hotmail.com>
2025-03-20 12:15:14 +0100

    refactor newstudent page

---
+++ src/content/text/sv/newstudent.ts
@@ -0,0 +1,19 @@
+export const text = {
+    title: "Grattis till antagningen,",
+    subTitle: "och välkommen till Sveriges bästa studentstad: Lund!", 
+    info: "Det är ofta många frågor man har som ny student. Nedan har vi försökt samla viktig information och länkar.",
+    nollning: "Nollning",
+    nollningInfo: `Nollningen kan beskrivas som en 6 veckor lång introduktion till studietiden och livet som student.
+                Det är en intensiv period fylld med olika former av evenemang, som ger en vänner och minnen för livet. 
+                Att vara med på nollningen är helt frivilligt, och du är med på så mycket eller lite som du vill.`,
+    welcomingLunch: "Välkomstlunch",
+    welcomingLunchInfo: `Dagen innan studiestart anordnar D-Chip traditionsenligt en välkomstlunch på campus, 
+                där du som nyantagen kvinna eller icke-binär till datateknik eller information- & kommunikationsteknik är välkommen. 
+                Lunchen är ett perfekt tillfälle att träffa både andra nyantagna samt äldre studenter.
+                Mer information om välkomstlunchen kommer ut någon gång i början på augusti.`,
+    mentorship: "Mentorskapsprogrammet",
+    mentorshipInfo: `Under höstterminen anordnar D-Chip ett mentorskapsprogram där du som ny student får möjlighet att knyta kontakt med äldre kvinnliga och icke-binära studenter som pluggar samma sak som dig. Programmet är tänkt att fungera som ett stöd för kvinnor och icke-binära på D-sektionen i början av utbildningen och syftar till att skapa en trygg och välkomnande miljö.\n
+    Mentorskapsprogrammet är ett utmärkt tillfälle att lära känna fler tjejer och icke-binära på sektionen. Förutom att du tilldelas en mentor, kommer det även hållas evenemang och aktiviteter där alla microchip och mentorer som deltar i programmet samlas och lär känna varandra.\n
+    Tillsammans med din mentor har du möjlighet att prata om allt från studentlivet och studierna, till livet i Lund i allmänhet. Mentorerna har tystnadsplikt, vilket innebär att du kan känna dig trygg med att allt som delas stannar mellan dig och din mentor.\n
+    Information om hur du anmäler dig till mentorskapsprogrammet kommer ut någon gång under hösten.`
+};
