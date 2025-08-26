de7d7cda8cf170a2234b3a5266b5158986d7a265
melissaengberg <me8614en-s@student.lu.se>
2025-08-26 12:04:48 +0200

    Pictures of the new Board members added

--- src/content/text/en/board.ts
+++ src/content/text/en/board.ts
@@ -1,134 +1,147 @@
-const president = [ 
-    {
-        name: "Hilma Larsson",
-        position: "President",
-        class: "C23",
-        description: "The president is responsible for, in consultation with the board, leading the association, as well as representing the association in official contexts.",
-        contact: "d-chipordforande@dsek.se",
-        image: "/images/board/NoPicture.jpg" // Add the actual image path
-      }
-    ]
-    const teamMembers = [
-      {
-        name: "Hanna Arendt",
-        position: "Vice President",
-        class: "C23",
-        description: "The vice president is responsible for representing the association in the absence of the president, as well as performing tasks that align with the association's purpose, as delegated by the president.",
-        contact: "d-chipvordforande@dsek.se",
-        image: "/images/board/hanna.jpg" // image path
-      },
-      {
-        name: "Ylva Lengborg",
-        position: "Event manager",
-        class: "C23",
-        description: "The event manager is primarily responsible for the events organized by the association and for managing the association's sellable items.",
-        contact: "d-chipevent@dsek.se",
-        image: "/images/board/ylva.jpg" // image path
-      },
-      {
-        name: "Lisa Clevestam",
-        position: "Head of Communications",
-        class: "C23",
-        description: "The Head of Communications is responsible for the association's website and social media. They are also in charge of distributing internal information related to D-Chip.",
-        contact: "d-chipinfo@dsek.se",
-        image: "/images/board/NoPicture.jpg" // image path
-      },
-      {
-        name: "Elly Hedin Oleniusson",
-        position: "Head of Corporate Relations",
-        class: "D23",
-        description: "The Head of Corporate Relations is responsible for managing and initiating the association's corporate contacts.",
-        contact: "d-chipnaringsliv@dsek.se",
-        image: "/images/board/elly.jpg" // image path
-      }, 
-      {
-        name: "Vivea Upadhyaya",
-        position: "Treasurer",
-        class: "D24",
-        description: "The treasurer is responsible for, in consultation with the board, overseeing the association's finances.",
-        contact: "d-chipekonomi@dsek.se",
-        image: "/images/board/NoPicture.jpg" // image path
-      },
-      {
-      name: "Maya Frank",
-        position: "Secretary",
-        class: "C24",
-        description: "The secretary is responsible for taking minutes at all association meetings and board meetings, as well as ensuring that the protocols are approved and published in an appropriate manner.",
-        contact: "d-chipsekreterare@dsek.se",
-        image: "/images/board/NoPicture.jpg" // image path
-      },
-    
-    ];
-    const revisorer = [
-    {
-        name: "Kristina Sedelius",
-        position: "Revisor",
-        class: "D21",
-        description: "A passionate learner and aspiring developer.",
-        contact: "d-chiprevisor@dsek.se",
-        image: "/images/board/hanna.jpg" // Add the actual image path
-      },
-      {
-        name: "Ester Nielsen Gulis",
-        position: "Revisor",
-        class: "C22",
-        description: "A passionate learner and aspiring developer.",
-        contact: "d-chiprevisor@dsek.se",
-        image: "/images/board/hanna.jpg" // Add the actual image path
-      },
-    ];
-    const valberedning = [
-    {
-        name: "Clara Schagerström",
-        position: "Valberedning",
-        class: "D23",
-        description: "A passionate learner and aspiring developer.",
-        contact: "d-chiprevisor@dsek.se",
-        image: "/images/board/hanna.jpg" // Add the actual image path
-      },
-      {
-        name: "Elsa Ryrlén",
-        position: "Valberedning",
-        class: "C21",
-        description: "Föreningens valberedning utgörs av en representant från var och en av de fyra senaste årskurserna på Datateknik och Infocom. Valberedningen utgörs av minst två representanter.",
-        contact: "d-chiprevisor@dsek.se",
-        image: "/images/board/hanna.jpg" // Add the actual image path
-      },
-      {
-        name: "Julia Blomgren",
-        position: "Valberedning",
-        class: "D21",
-        description: "A passionate learner and aspiring developer.",
-        contact: "d-chiprevisor@dsek.se",
-        image: "/images/board/hanna.jpg" // Add the actual image path
-      }
-    ];
-    
-    const jubileegroup = [
-    {
-        name: "Anna Hallin",
-        class: "C23",
-      },
-      {
-        name: "Alice Ward",
-        class: "D24",
-      },
-      {
-        name: "Alma Karlsson",
-        class: "C23",
-      },
-      {
-        name: "Astrid Mildner",
-        class: "C23",
-      },
-      {
-        name: "Ellen Persson",
-        class: "C23",
-      },
-      {
-      name: "Linn Eripe",
-        class: "C23",
-      }
-    ];
+const president = [
+  {
+    name: "Hilma Larsson",
+    position: "President",
+    class: "C23",
+    description:
+      "The president is responsible for, in consultation with the board, leading the association, as well as representing the association in official contexts.",
+    contact: "d-chipordforande@dsek.se",
+    image: "/images/board/Hilma.jpg", // Add the actual image path
+  },
+];
+const teamMembers = [
+  {
+    name: "Melissa Engberg",
+    position: "Vice President",
+    class: "D21",
+    description:
+      "The vice president is responsible for representing the association in the absence of the president, as well as performing tasks that align with the association's purpose, as delegated by the president.",
+    contact: "d-chipvordforande@dsek.se",
+    image: "/images/board/Melissa.jpg", // image path
+  },
+  {
+    name: "Philippa Antunes",
+    position: "Event manager",
+    class: "C23",
+    description:
+      "The event manager is primarily responsible for the events organized by the association and for managing the association's sellable items.",
+    contact: "d-chipevent@dsek.se",
+    image: "/images/board/Philippa.jpg", // image path
+  },
+  {
+    name: "Lisa Clevestam",
+    position: "Head of Communications",
+    class: "C23",
+    description:
+      "The Head of Communications is responsible for the association's website and social media. They are also in charge of distributing internal information related to D-Chip.",
+    contact: "d-chipinfo@dsek.se",
+    image: "/images/board/Lisa.jpg", // image path
+  },
+  {
+    name: "Diana Al Allaf",
+    position: "Head of Corporate Relations",
+    class: "D24",
+    description:
+      "The Head of Corporate Relations is responsible for managing and initiating the association's corporate contacts.",
+    contact: "d-chipnaringsliv@dsek.se",
+    image: "/images/board/Diana.jpg", // image path
+  },
+  {
+    name: "Vivea Upadhyaya",
+    position: "Treasurer",
+    class: "D24",
+    description:
+      "The treasurer is responsible for, in consultation with the board, overseeing the association's finances.",
+    contact: "d-chipekonomi@dsek.se",
+    image: "/images/board/Vivea.jpg", // image path
+  },
+  {
+    name: "Maya Frank",
+    position: "Secretary",
+    class: "C24",
+    description:
+      "The secretary is responsible for taking minutes at all association meetings and board meetings, as well as ensuring that the protocols are approved and published in an appropriate manner.",
+    contact: "d-chipsekreterare@dsek.se",
+    image: "/images/board/Maya.jpg", // image path
+  },
+];
+const revisorer = [
+  {
+    name: "Kristina Sedelius",
+    position: "Revisor",
+    class: "D21",
+    description: "A passionate learner and aspiring developer.",
+    contact: "d-chiprevisor@dsek.se",
+    image: "/images/board/hanna.jpg", // Add the actual image path
+  },
+  {
+    name: "Ester Nielsen Gulis",
+    position: "Revisor",
+    class: "C22",
+    description: "A passionate learner and aspiring developer.",
+    contact: "d-chiprevisor@dsek.se",
+    image: "/images/board/hanna.jpg", // Add the actual image path
+  },
+];
+const valberedning = [
+  {
+    name: "Clara Schagerström",
+    position: "Valberedning",
+    class: "D23",
+    description: "A passionate learner and aspiring developer.",
+    contact: "d-chiprevisor@dsek.se",
+    image: "/images/board/hanna.jpg", // Add the actual image path
+  },
+  {
+    name: "Elsa Ryrlén",
+    position: "Valberedning",
+    class: "C21",
+    description:
+      "Föreningens valberedning utgörs av en representant från var och en av de fyra senaste årskurserna på Datateknik och Infocom. Valberedningen utgörs av minst två representanter.",
+    contact: "d-chiprevisor@dsek.se",
+    image: "/images/board/hanna.jpg", // Add the actual image path
+  },
+  {
+    name: "Julia Blomgren",
+    position: "Valberedning",
+    class: "D21",
+    description: "A passionate learner and aspiring developer.",
+    contact: "d-chiprevisor@dsek.se",
+    image: "/images/board/hanna.jpg", // Add the actual image path
+  },
+];
 
-export const board = { president, teamMembers, revisorer, valberedning, jubileegroup };
+const jubileegroup = [
+  {
+    name: "Anna Hallin",
+    class: "C23",
+  },
+  {
+    name: "Alice Ward",
+    class: "D24",
+  },
+  {
+    name: "Alma Karlsson",
+    class: "C23",
+  },
+  {
+    name: "Astrid Mildner",
+    class: "C23",
+  },
+  {
+    name: "Ellen Persson",
+    class: "C23",
+  },
+  {
+    name: "Linn Eripe",
+    class: "C23",
+  },
+];
+
+export const board = {
+  president,
+  teamMembers,
+  revisorer,
+  valberedning,
+  jubileegroup,
+};
