d93ea2ac2abf90264e7370414f48d4f8ef6028f3
melissaengberg <me8614en-s@student.lu.se>
2025-10-10 11:28:07 +0200

    Divided up past and coming events in the event page

--- src/content/text/en/event.ts
+++ src/content/text/en/event.ts
@@ -1,4 +1,6 @@
 export const text = {
-    title: "Events",
-    info: "Here you can see upcoming events. You can find registration and more information in the event descriptions."
+  title: "Events",
+  info: "Here you can see upcoming events. You can find registration and more information in the event descriptions.",
+  titlePast: "Past Events",
+  infoPast: "Here you can see the past events we've had.",
 };
--- src/content/text/sv/event.ts
+++ src/content/text/sv/event.ts
@@ -1,4 +1,6 @@
 export const text = {
-    title: "Evenemang",
-    info: "Här kan du se våra kommande event. Anmälan och mer information hittar du i eventbeskrivningarna."
+  title: "Evenemang",
+  info: "Här kan du se våra kommande event. Anmälan och mer information hittar du i eventbeskrivningarna.",
+  titlePast: "Tidigare evenemang",
+  infoPast: "Här kan du se våra tidigare evenemang.",
 };
